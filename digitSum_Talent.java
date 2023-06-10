6a6fabb1e2b2332145495e0509440277978fcdfb
Gauri Agrawal <73654083+Gauri-Agrawal838@users.noreply.github.com>
2023-06-10 22:05:59 +0530

    Add files via upload

---
+++ digitSum_Talent.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+	public int nonRepeatDigitsCount(int input1){
+
+		// Write code here...
+        
+		while(input1/10!=0)
+        {
+            int sum=0;
+            while(input1!=0)
+            {
+                sum += input1%10;
+                input1 /= 10;
+            }
+            input1 = sum;
+        }
+        return input1;
+	}
+}
